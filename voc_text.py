68e45e3502246fd9eceea9f5f2d8e3ee938e12ad
fsx950223 <fsx950223@outlook.com>
2019-06-06 16:49:38 +0800

    feat(text): add text generator

--- voc_text.py
+++ voc_text.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import os
 import threading
 import glob
+from io import StringIO
+import shutil
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 if hasattr(tf, 'enable_eager_execution'):
     tf.enable_eager_execution()
 
@@ -16,17 +19,19 @@ FLAGS = parser.parse_args()
 xmls=glob.glob(os.path.join(FLAGS.folder,'**/*.xml'),recursive=True)
 chunk_num=len(xmls)//threads
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-train_file = tf.io.gfile.GFile('train.txt', 'w')
-val_file = tf.io.gfile.GFile('val.txt', 'w')
-test_file = tf.io.gfile.GFile('test.txt', 'w')
+train_file = StringIO()
+val_file = StringIO()
+test_file = StringIO()
 nums=[0,0,0]
 splits=[0.8,0.9,1]
 mutex = threading.Lock()
 def write_xmls(xmls):
     for xml_path in xmls:
         image_path=glob.glob(os.path.join('/'.join(xml_path.split('/')[:-2]),'**',xml_path.split('/')[-1].split('.')[0]+'.jp*g'),recursive=True)[0]
+        if tf.io.gfile.exists(image_path) is not True:
+            continue
         rand = tf.random.uniform([],0,1)
-        xml_root=ET.parse(xml_path.encode('utf-8')).getroot()
+        xml_root=ET.parse(xml_path).getroot()
         if rand < splits[0]:
             file=train_file
             index=0
@@ -37,6 +42,7 @@ def write_xmls(xmls):
             file=test_file
             index = 2
         label=image_path
+        objects=0
         for obj in xml_root.iter('object'):
             difficult = obj.find('difficult').text
             cls = obj.find('name').text
@@ -47,6 +53,9 @@ def write_xmls(xmls):
             b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text),
                  int(xmlbox.find('ymax').text))
             label+=' '+' '.join([str(a) for a in b])+' '+ str(cls_id)
+            objects+=1
+        if objects<=0:
+            continue
         label+='\n'
         mutex.acquire()
         file.write(label)
@@ -63,6 +72,12 @@ for thread in pool:
 train_file.close()
 val_file.close()
 test_file.close()
-tf.io.gfile.rename('train.txt','voc_train_'+str(nums[0])+'.txt')
-tf.io.gfile.rename('val.txt','voc_val_'+str(nums[1])+'.txt')
-tf.io.gfile.rename('test.txt','voc_test_'+str(nums[2])+'.txt')
+with open ('voc_train_'+str(nums[0])+'.txt', 'w') as fd:
+    train_file.seek(0)
+    shutil.copyfileobj(train_file, fd)
+with open ('voc_val_'+str(nums[1])+'.txt', 'w') as fd:
+    val_file.seek(0)
+    shutil.copyfileobj(val_file, fd)
+with open ('voc_test_'+str(nums[2])+'.txt', 'w') as fd:
+    test_file.seek(0)
+    shutil.copyfileobj(test_file, fd)
