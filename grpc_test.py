96a068573a5a6cee54f61dd5c8b20ff909d27240
fsx950223 <fsx950223@outlook.com>
2019-06-09 12:17:53 +0800

    feat(update); update grpc tesr

--- grpc_test.py
+++ grpc_test.py
@@ -2,25 +2,30 @@ from PIL import Image, ImageFont, ImageDraw
 import threading
 import os
 import time
+import psutil
 
 SIZE=416
 FOLDER='/media/fangsixie/data/keras-yolo3/logs/VOCdevkit/Illegal_parking/'
 CLASSES_PATH = os.path.expanduser('model_data/cci.names')
 SAVE_PATH='chengyun'
+THREADS=10
+EPOCHS=10
 
+results=[]
 mutex=threading.Lock()
 index=-1
-time=0
 def predict_wrapper(client,image_names,save=False):
     mutex.acquire()
     index+=1
     mutex.release()
     image_path=os.path.join(folder,image_names[index])
     image = Image.open(image_path)
-    start=int(round(time.time() * 1000))
-    out_classes,out_scores,out_boxes=client.predict(image)
-    end=int(round(time.time() * 1000))
-    time+=end-start
+    
+    for epoch in range(EPOCHS):
+        start=int(round(time.time() * 1000))
+        out_classes,out_scores,out_boxes=client.predict(image)
+        end=int(round(time.time() * 1000))
+        results.append(end-start)
     if save:
         save_image(image,out_classes,out_scores,out_boxe)
     else:
@@ -65,6 +70,12 @@ def save_image(image,out_classes,out_scores,out_boxes):
             draw.text(text_origin, label, fill=(0, 0, 0), font=font)
         del draw
         image.save(os.path.join(SAVE_PATH,image_name))
+def print_system_info():
+    pid=os.getpid()
+    while True:
+        process=psutil.Process(pid)
+        print("memory: ",process.memory_percent())
+        print("cpu: ",process.cpu_percent(None))
 
 def main(_):
     os.mkdir(SAVE_PATH)
@@ -74,16 +85,24 @@ def main(_):
     class_names = [c.strip() for c in class_names]
     client=Client('10.12.102.32:8000',class_names,SIZE)
     threads = []
-    for i in range(len(names)):
+    info_thread=threading.Thread(target=print_system_info)
+    info_thread.start()
+    for i in range(THREADS):
         thread = threading.Thread(target=predict_wrapper, args=[image])
         threads.append(thread)
-    #start=int(round(time.time() * 1000))
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
-    #end = int(round(time.time() * 1000))
-    print(time)
+    total_time=0
+    results=results.sort()
+    for result in results:
+        total_time+=result
+    average_time=total_time/len(results)
+    print("average time: ",average_time)
+    print("tps: ",1/average_time)
+    print("min time: ",results[0])
+    print("max time: ",results[-1])
 
 if __name__ == '__main__':
     main(None)
